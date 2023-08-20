de0276554e76d3a99193fe731fe6a40a662be97f
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-08-20 14:17:09 -0700

    improve handling of default college

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from course import Course
 from search import search, CourseNotFound
 
 app = FastAPI()
+
+
+class NoCollegeFound(Exception):
+    pass
+
+
+def split_course(s: str) -> str:
+    course = s.rstrip("0123456789")
+
+    if not course:
+        raise NoCollegeFound
+    else:
+        course = course.upper()
+
+    course_num = s[len(course) :]
+    return course + course_num
 
 
 @app.get("/")
@@ -15,17 +32,16 @@ async def ping():
     return {"message": "pong"}
 
 
-@app.get("/api/search/")
+@app.get("/api/course/{course}")
 async def get_course(course: str):
+    try:
+        course = split_course(course)
+    except NoCollegeFound:
+        return RedirectResponse(url="/api/course/CPSC" + course)
+
     try:
         course_model: Course = search(course)
     except CourseNotFound:
         return {"error": "Course not found"}
 
-    return {
-        "title": course_model.title,
-        "description": course_model.description,
-        "prerequisites": course_model.prereqs,
-        "corequisites": course_model.coreqs,
-        "url": course_model.url,
-    }
+    return course_model.json()
--- search.py
+++ search.py
@@ -5,21 +5,8 @@ from course import Course, CourseNotFound
 BASE_URL = "https://catalog.fullerton.edu"
 
 
-def split_course(s: str) -> str:
-    course = s.rstrip("0123456789")
-
-    if not course:
-        return "CPSC " + s
-    else:
-        course = course.upper()
-
-    course_num = s[len(course) :]
-    return course + course_num
-
-
 def search(query: str) -> Course:
-    course = split_course(query)
-    URL = f"{BASE_URL}/search_advanced.php?cur_cat_oid=80&ecpage=1&cpage=1&ppage=1&pcpage=1&spage=1&tpage=1&search_database=Search&filter%5Bkeyword%5D={course}&filter%5Bexact_match%5D=1&filter%5B3%5D=1&filter%5B31%5D=1"
+    URL = f"{BASE_URL}/search_advanced.php?cur_cat_oid=80&ecpage=1&cpage=1&ppage=1&pcpage=1&spage=1&tpage=1&search_database=Search&filter%5Bkeyword%5D={query}&filter%5Bexact_match%5D=1&filter%5B3%5D=1&filter%5B31%5D=1"
     soup = BeautifulSoup(requests.get(URL).content, "html.parser")
 
     if (COURSE_URL := soup.find("a", {"aria-expanded": "false"})["href"]) is None:

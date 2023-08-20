591c252e1ccc50dfb0fa374abc8bbc7d8f3e562c
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-08-20 12:51:47 -0700

    simplify course input

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 
-COURSE_COLL = input("Enter a course college: ")
-COURSE_NUM = input("Enter a course number: ")
-if COURSE_COLL == "":
-    COURSE_COLL = "CPSC"
-COURSE = COURSE_COLL + " " + COURSE_NUM
-URL = f"https://catalog.fullerton.edu/search_advanced.php?cur_cat_oid=80&ecpage=1&cpage=1&ppage=1&pcpage=1&spage=1&tpage=1&search_database=Search&filter%5Bkeyword%5D={COURSE}&filter%5Bexact_match%5D=1&filter%5B3%5D=1&filter%5B31%5D=1"
+BASE_URL = "https://catalog.fullerton.edu"
 
+
+def split_course(s: str) -> str:
+    course = s.rstrip("0123456789")
+
+    if not course:
+        return "CPSC " + s
+    else:
+        course = course.upper()
+
+    course_num = s[len(course) :]
+    return course + course_num
+
+
+course = split_course(input("Enter course: "))
+URL = f"{BASE_URL}/search_advanced.php?cur_cat_oid=80&ecpage=1&cpage=1&ppage=1&pcpage=1&spage=1&tpage=1&search_database=Search&filter%5Bkeyword%5D={course}&filter%5Bexact_match%5D=1&filter%5B3%5D=1&filter%5B31%5D=1"
 soup = BeautifulSoup(requests.get(URL).content, "html.parser")
 
-COURSE_URL = soup.find("a", {"aria-expanded": "false"})["href"]
-
-RESULT_URL = f"https://catalog.fullerton.edu/{COURSE_URL}"
-soup = BeautifulSoup(requests.get(RESULT_URL).content, "html.parser")
-
-content = soup.find("td", {"class": "block_content"})
-
-if content is None:
+if (COURSE_URL := soup.find("a", {"aria-expanded": "false"})["href"]) is None:
     print("Course not found")
     exit()
 
+RESULT_URL = f"{BASE_URL}/{COURSE_URL}"
+soup = BeautifulSoup(requests.get(RESULT_URL).content, "html.parser")
+
+content = soup.find("td", {"class": "block_content"})
 title = content.find("h1").text
 
 # get text before first line break

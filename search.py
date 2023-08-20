11057832e30a582fe06b034394a95b543d77c5bf
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-08-20 14:59:36 -0700

    fix nonetype error

--- search.py
+++ search.py
@@ -9,7 +9,8 @@ def search(query: str) -> Course:
     URL = f"{BASE_URL}/search_advanced.php?cur_cat_oid=80&ecpage=1&cpage=1&ppage=1&pcpage=1&spage=1&tpage=1&search_database=Search&filter%5Bkeyword%5D={query}&filter%5Bexact_match%5D=1&filter%5B3%5D=1&filter%5B31%5D=1"
     soup = BeautifulSoup(requests.get(URL).content, "html.parser")
 
-    if (COURSE_URL := soup.find("a", {"aria-expanded": "false"})["href"]) is None:
-        raise CourseNotFound(f"Course {course} not found")
+    if (element := soup.find("a", {"aria-expanded": "false"})) is None:
+        raise CourseNotFound(f"Course {query} not found")
 
+    COURSE_URL = element["href"]
     return Course(f"{BASE_URL}/{COURSE_URL}")
